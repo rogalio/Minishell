e9ee7692c62afd13f0be0752c565b80745d90950
Camelia ABDLI <cabdli@paul-f5Br1s3.clusters.42paris.fr>
2024-04-11 10:39:46 +0200

    [TEST] - token_list behavior testing in case of malloc error

--- src/prompt/shell.c
+++ src/prompt/shell.c
@@ -6,7 +6,7 @@
 /*   By: cabdli <cabdli@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 19:02:05 by rogalio           #+#    #+#             */
-/*   Updated: 2024/04/09 17:29:17 by cabdli           ###   ########.fr       */
+/*   Updated: 2024/04/11 10:28:36 by cabdli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,19 @@ static int	parse_input(t_minishell *minishell, char *input)
 	if (!(minishell->token_list))
 		return (print_err_msg(&(minishell->error)), 0);
 	print_token_list(minishell->token_list);
-	if (!syntax_analyzer(minishell->token_list, &(minishell->error)))
-	{
-		print_err_msg(&(minishell->error));
-		return (free_token_list(minishell->token_list), 0);
-	}
-	minishell->pipeline = create_pipeline(minishell->token_list, \
-	minishell->data->env, &(minishell->error));
-	if (!minishell->pipeline)
-	{
-		print_err_msg(&(minishell->error));
-		return (free_token_list(minishell->token_list), 0);
-	}
-	print_pipeline(minishell->pipeline);
+	// if (!syntax_analyzer(minishell->token_list, &(minishell->error)))
+	// {
+	// 	print_err_msg(&(minishell->error));
+	// 	return (free_token_list(minishell->token_list), 0);
+	// }
+	// minishell->pipeline = create_pipeline(minishell->token_list, \
+	// minishell->data->env, &(minishell->error));
+	// if (!minishell->pipeline)
+	// {
+	// 	print_err_msg(&(minishell->error));
+	// 	return (free_token_list(minishell->token_list), 0);
+	// }
+	// print_pipeline(minishell->pipeline);
 	return (1);
 }
 
