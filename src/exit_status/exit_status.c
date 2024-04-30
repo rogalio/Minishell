c484310a1ee21d1b9852da1085bdcf848eec1f76
Camelia ABDLI <cabdli@paul-f5Br5s1.clusters.42paris.fr>
2024-04-30 16:48:11 +0200

    [PROGRESS] - exit_status handling done until heredocs, left = exec

---
+++ src/exit_status/exit_status.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit_status.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cabdli <cabdli@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/24 17:29:07 by cabdli            #+#    #+#             */
+/*   Updated: 2024/04/30 15:31:46 by cabdli           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "exit_status.h"
+
+int	exit_process(t_minishell *minishell)
+{
+	int	exit_status;
+
+	exit_status = minishell->exit_status;
+	if (exit_status == UNEXPEC_ERR)
+	{
+		minishell->error = MALLOC;
+		print_err_msg(&(minishell->error));
+	}
+	if (g_exit_signal)
+	{
+		exit_status = g_exit_signal;
+		g_exit_signal = 0;
+	}
+	free_resources(minishell);
+	exit(exit_status);
+}
+
+int	exit_all(t_minishell *minishell)
+{
+	int	exit_status;
+
+	exit_status = minishell->exit_status;
+	free_minishell(&minishell);
+	return (exit_status);
+}
