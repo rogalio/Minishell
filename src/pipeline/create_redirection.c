8f33c9ceb6201a4428a2e8667d6ed082001f789c
Camelia ABDLI <cabdli@paul-f5Br5s6.clusters.42paris.fr>
2024-05-02 17:15:48 +0200

    [FEAT] - multi redirections handled

--- src/pipeline/create_redirection.c
+++ src/pipeline/create_redirection.c
@@ -6,11 +6,28 @@
 /*   By: cabdli <cabdli@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 16:44:08 by cabdli            #+#    #+#             */
-/*   Updated: 2024/04/22 14:56:14 by cabdli           ###   ########.fr       */
+/*   Updated: 2024/05/02 17:12:34 by cabdli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipeline.h"
+
+static int	open_redirection(t_redirection *redirect)
+{
+	int	fd;
+
+	fd = 0;
+	if (!ft_strcmp(redirect->type, "<"))
+		return (0);
+	fd = open(redirect->file, O_RDONLY | O_CREAT, 0644);
+	if (fd == -1)
+	{
+		ft_putstr_fd("minishell:", STDERR_FILENO);
+		return (perror(redirect->file), 1);
+	}
+	close(fd);
+	return (0);
+}
 
 static t_redirection	*init_redirection(void)
 {
@@ -43,6 +60,8 @@ const char *file, t_env *env)
 	if (!(*redirect)->file)
 		return (0);
 	if (!handle_expand_quotes(&((*redirect)->file), env))
+		return (0);
+	if (open_redirection(*redirect))
 		return (0);
 	return (1);
 }
